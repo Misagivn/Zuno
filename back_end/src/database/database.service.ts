ee0dd4fee68cdad69b9d4ebb1154cbcbcb3d57e4
Minh Nhat Luong <75418809+Misagivn@users.noreply.github.com>
2025-05-15 14:52:07 +0700

    completed prisma setup. immgrate DB to file.
    completed prisma setup. immgrate DB to file.

---
+++ back_end/src/database/database.service.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable prettier/prettier */
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleInit {
+    async onModuleInit() {
+        await this.$connect()
+            .then(() => {
+                console.log('Database connected');
+            })
+            .catch((error) => {
+                console.error('Database connection error:', error);
+            });
+    }
+}
